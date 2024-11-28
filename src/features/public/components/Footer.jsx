458a45d5cde66ecb47d5443f2a35e9c7994252c7
Nandarlin25 <nandarlin2511@gmail.com>
2024-11-28 21:35:03 +0630

    added base structure for frame handling

--- src/features/public/components/Footer.jsx
+++ src/features/public/components/Footer.jsx
@@ -1,59 +1,75 @@
 import React from "react";
+import Container from "../../../components/Container";
 
 const Footer = () => {
   return (
-    <footer className="border-t-2 border-gray-200 antialiased dark:bg-gray-800 mt-auto">
-      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+   
+<footer className="bg-stone-900 text-gray-400 py-8 mt-auto">
+   <Container>
+   <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-4 gap-8">
+    {/* Brand Section */}
+    <div className="">
+    <h2 className="text-white text-2xl font-bold">Emerald</h2>
+      <p className="mt-4 leading-8">
+        Beautiful room presentations, straightforward booking &amp; reservation
+        options, &amp; a whole lot more awaits here.
+      </p>
+    </div>
+    {/* Explore Links */}
+    <div className="px-24">
+      <h3 className="text-white text-lg font-semibold">Explore</h3>
+      <ul className="mt-4 space-y-2">
+        <li><a href="#" className="hover:text-white">Home</a></li>
+        <li><a href="#" className="hover:text-white">About us</a></li>
+        <li><a href="#" className="hover:text-white">Blog</a></li>
+        <li><a href="#" className="hover:text-white">Services</a></li>
+      </ul>
+    </div>
+    {/* Contact Links */}
+    <div className="px-8">
+      <h3 className="text-white text-lg font-semibold">Contact</h3>
+      <ul className="mt-4 space-y-2">
+        <li><a href="#" className="hover:text-white">Contact Us</a></li>
+        <li><a href="#" className="hover:text-white">FAQ</a></li>
+        <li><a href="#" className="hover:text-white">Privacy Policy</a></li>
+        <li><a href="#" className="hover:text-white">Terms &amp; conditions</a></li>
+      </ul>
+    </div>
+    {/* App & Social Media */}
+    <div className="">
+      <h3 className="text-white text-lg font-semibold">App available on</h3>
+      <div className="mt-4 flex space-x-4">
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/1280px-Google_Play_Store_badge_EN.svg.png" alt="Google Play" className="w-28 h-10" /></a>
+      
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/2560px-Download_on_the_App_Store_Badge.svg.png" alt="App Store" className="w-28 h-10" /></a>
+
         
-        <div className="py-6 md:py-8">
-          <div className="gap-4 space-y-5 xl:flex xl:items-center xl:justify-between xl:space-y-0">
-            <a href="#" className="block">
-              <img
-                className="block h-8 w-auto dark:hidden"
-                src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full.svg"
-              />
-              <img
-                className="hidden h-8 w-auto dark:block"
-                src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full-dark.svg"
-              />
-            </a>
-            <ul className="flex flex-wrap items-center gap-4 text-sm text-gray-900 dark:text-white xl:justify-center">
-              <li>
-                <a href="#" className="font-medium hover:underline">
-                  {" "}
-                  Flowbite Express{" "}
-                </a>
-              </li>
-              <li>
-                <a href="#" className="font-medium hover:underline">
-                  {" "}
-                  Legal Notice{" "}
-                </a>
-              </li>
-              <li>
-                <a href="#" className="font-medium hover:underline">
-                  {" "}
-                  Product Listing Policy{" "}
-                </a>
-              </li>
-              <li>
-                <a href="#" className="font-medium hover:underline">
-                  {" "}
-                  Terms of Use{" "}
-                </a>
-              </li>
-            </ul>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              © 2024{" "}
-              <a href="#" className="hover:underline">
-                Flowbite
-              </a>
-              , Inc. All rights reserved.
-            </p>
-          </div>
+      </div>
+      <h3 className="text-white text-lg font-semibold mt-6">Follow Us</h3>
+      <div className="mt-4 flex space-x-4">
+        <div className="bg-black size-10 flex justify-center items-center rounded-lg">
+        <a href="#" className="hover:text-white"><i class="fa-brands fa-facebook-f text text-white text-lg"></i></a>
+        </div>
+
+        <div className="bg-black size-10 flex justify-center items-center rounded-lg ">
+        <a href="#" className="hover:text-white"><i class="fa-brands fa-instagram text text-white text-lg"></i></a>
+        </div>
+
+        <div className="bg-black size-10 flex justify-center items-center rounded-lg">
+        <a href="#" className="hover:text-white"><i class="fa-brands fa-twitter text text-white text-lg"></i></a>
+        </div>
+        
+        <div className="bg-black size-10 flex justify-center items-center rounded-lg">
+        <a href="#" className="hover:text-white"><i class="fa-brands fa-linkedin-in text text-white text-lg"></i></a>
         </div>
       </div>
-    </footer>
+    </div>
+  </div>
+  <div className="mt-8 border-t border-gray-700 pt-4 text-start">
+    <p className="py-2">Copyrights ©2024. All Rights Reserved.</p>
+  </div>
+   </Container>
+</footer>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,49 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
+    // fontFamily: {
+    //   sans: ["Montserrat", "Padauk", "sans-serif"],
+    //   },
     fontFamily: {
       sans: ["Montserrat", "Padauk", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      body: ["Hanken Grotesk", "sans-serif"],
+      caveat: ["Caveat", "cursive"],
+      heading: ["Playfair Display", "serif"],
     },
     extend: {
       fontFamily: {
         heading: ["Poppins", "sans-serif"],
       },
+      colors: {
+        primary: {
+          DEFAULT: "#18181B", // Primary dark color
+          frame: "#FAFAFA", // Light frame color
+        },
+        secondary: {
+          // Fixed typo from 'secondaty' to 'secondary'
+          DEFAULT: "#71717A", // Secondary dark color
+          card: "#F4F4F5", // Light card section color
+        },
+        footer: {
+          DEFAULT: "#27272A", // Default footer color
+        },
+        nav: {
+          DEFAULT: "#A1A1AA", // Default navigation color
+          zic300: "#D4D4D8", // Additional nav color
+        },
+        border: {
+          DEFAULT: "#D4D4D8", // Default border color
+        },
+        stroke: {
+          DEFAULT: "#FFE338", // Default stroke color
+        },
+        orderComplete: {
+          DEFAULT: "#00E510", // Color indicating completed orders
+        },
+        // Add more color categories as needed
+      },
     },
-  },
+    },
   plugins: [require("flowbite/plugin")],
 };
