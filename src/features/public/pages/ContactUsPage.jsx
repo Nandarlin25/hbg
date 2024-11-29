34e81148c76a72a4b46ba382f6cca273e5e178e3
PanOu7 <pou214518@gmail.com>
2024-11-30 01:41:40 +0630

    feat:contactus

--- src/features/public/pages/ContactUsPage.jsx
+++ src/features/public/pages/ContactUsPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Container from '../../../components/Container'
 import BreadCrumb from '../components/Breadcrumb'
+import ContactUs from '../components/ContactUs'
 
 const ContactUsPage = () => {
   return (
     <>
     <Container>
       <BreadCrumb currentPageTitle={"Contact Us"} />
+      <ContactUs/>
     </Container>
     </>
   )
