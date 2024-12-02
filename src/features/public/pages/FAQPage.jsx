ae615c8276533a4a7233572b9980b645389d304a
PanOu7 <pou214518@gmail.com>
2024-12-02 14:46:54 +0630

    config:footerLink

---
+++ src/features/public/pages/FAQPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../../../components/Container';
+
+const FAQPage = () => {
+  return (
+    <Container>
+        Faq
+    </Container>
+  );
+}
+
+export default FAQPage;
---
+++ src/features/public/pages/TermAndConditionPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../../../components/Container';
+
+const TermAndConditionPage = () => {
+  return (
+  <Container>
+    Term and condition
+  </Container>
+  );
+}
+
+export default TermAndConditionPage;
