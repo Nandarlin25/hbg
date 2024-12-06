58d8e2599ba2dc931c6907684085ce4812cdb818
PanOu7 <pou214518@gmail.com>
2024-12-06 20:52:50 +0630

    fix:responsive update

--- src/data/gymData.js
+++ src/data/gymData.js
@@ -3,16 +3,8 @@ import card2 from "../assets/Service/Fitness Center/card 2.jpeg";
 import card3 from "../assets/Service/Fitness Center/card 3.jpeg";
 import card4 from "../assets/Service/Fitness Center/card 4.jpeg";
 export const gymData = [
-  {
-    image: card1,
-  },
-  {
-    image: card2,
-  },
-  {
-    image: card3,
-  },
-  {
-    image: card4,
-  },
+  { id: 1, image: card1 },
+  { id: 2, image: card2 },
+  { id: 3, image: card3 },
+  { id: 4, image: card4 },
 ];
